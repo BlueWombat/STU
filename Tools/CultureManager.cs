015a7632fe3edca8a9054637e74546c8248f7191
Mikkel Petersen <mikkel@mikkelp.com>
2017-02-27 00:43:12 +0100

    Created Tools project for holding CultureManager Centralized culture relevant code in CultureManager Added T4 Template for App.config in order to automatically list all cultures available for the language filter

---
+++ Tools/CultureManager.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace BlueWombat.Tools
+{
+    public sealed class CultureManager
+    {
+        public static List<Tuple<string, string>> GetCultures()
+        {
+            var cultureReg = new Regex(@"\s*\(.+\)$");
+            var cultures = CultureInfo.GetCultures(CultureTypes.SpecificCultures)
+            .Select(c => new
+            {
+                Name = c.EnglishName,
+                IsoCode = c.Name
+            })
+            .Union(CultureInfo.GetCultures(CultureTypes.SpecificCultures)
+                .Select(c => new
+                {
+                    Name = cultureReg.Replace(c.EnglishName, ""),
+                    IsoCode = c.TwoLetterISOLanguageName.ToLowerInvariant()
+                }))
+            .GroupBy(c => c.IsoCode)
+            .Distinct()
+            .Select(c => c.First())
+            .OrderBy(c => c.Name)
+            .Select(c => new Tuple<string, string>(c.Name, c.IsoCode))
+            .ToList();
+
+            return cultures;
+        }
+    }
+}
---
+++ UnitTests/CultureManagerTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using BlueWombat.Tools;
+using NUnit.Framework;
+
+namespace UnitTests
+{
+    [TestFixture]
+    public class CultureManagerTests
+    {
+        [Test]
+        public void can_we_get_all_cultures()
+        {
+            var cultures = CultureManager.GetCultures();
+            foreach (var culture in cultures)
+            {
+                Console.WriteLine($"Name: {culture.Item1} IsoCode; {culture.Item2}");
+            }
+        }
+    }
+}
